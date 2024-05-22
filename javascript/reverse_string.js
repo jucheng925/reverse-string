eb6c3498a2326ed6516321e33fe2c0889a590f22
Julie Cheng <jucheng925@gmail.com>
2024-05-22 14:05:11 -0400

    complete

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
   // type your code here
+  let reverse = ""
+  while (str.length > 0) {
+    reverse = reverse + str.slice(-1)
+    str = str.slice(0, (str.length -1))
+  }
+  return reverse
 }
 
 if (require.main === module) {
@@ -11,9 +17,32 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'ihcmik'");
+  console.log("=>", reverseString("kimchi"));
+
+  console.log("");
+
+  console.log("Expecting: 'i'");
+  console.log("=>", reverseString("i"));
+
+  console.log("");
+
+  console.log("Expecting: 'he8rt'");
+  console.log("=>", reverseString("tr8eh"));
+
+  console.log("");
+
+  console.log("Expecting: '333'");
+  console.log("=>", reverseString("333"));
+
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// create empty array variable
+// loop through and pop the last letter and then add into empty array
